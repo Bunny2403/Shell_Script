2bb50d41b85589e3562dc841d138dceeb81137d0
Athreya <mac@macs-MacBook-Pro.local>
2021-09-17 19:53:09 -0400

    Jenkins Script

---
+++ script.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+mkdir this
+
+echo "The present working directory is $PWD"
+
+cd this
+
+echo "The present working directory is $PWD"
+
+touch fec.docx
+
+chmod 741
+
+echo "The present working directory is $PWD"
+
+cat fec.docx
+
+echo "The present working directory is $PWD"
+
+echo "the script ends at"
+
+date
+
+
